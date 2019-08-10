ec9cb208379b31656dd0a59dca2fafeceb989a94
George Ruiz <georuiz817@gmail.com>
2019-08-10 04:13:50 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -41,6 +41,7 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
   user = User.find_by(username: params["username"])
+  
   if params[:username].empty?
       redirect '/failure'
     end
@@ -51,8 +52,7 @@ class ApplicationController < Sinatra::Base
       session[:user_id] = user.id 
       redirect '/account'
     end
-  
-end
+  end
 
   get "/failure" do
     erb :failure
